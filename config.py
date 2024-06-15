03b8e6adb6f838fb48a3d9210ab0f4ec4737ca70
krk <72791356+scrooge001@users.noreply.github.com>
2024-06-15 15:41:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1852829593))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Mrbot02")
+OWNER_ID = int(getenv("OWNER_ID", 7044258634))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/adgmovies")
