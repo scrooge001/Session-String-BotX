1753c47593fce07827ee274a9d6afb2eb4baa31a
krk <72791356+scrooge001@users.noreply.github.com>
2024-07-30 15:33:24 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://github.com/PREETCHAUHAN/Session-String-BotX"
+                text="sᴏᴜʀᴄᴇ", url="https://t.me/scrooge010"
             ),
         ],
     ]
